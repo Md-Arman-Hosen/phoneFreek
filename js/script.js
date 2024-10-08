9ddbc0688ad093b401f8feb635c78ed3cae8dd34
MD-Arman-Hosen <arman15-3304@diu.edu.bd>
2024-10-08 20:29:02 +0600

    Modal handle dynamicly

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const displayPhones = (phones,isShowAll) => {
     }
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // 2: create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
@@ -40,7 +40,7 @@ const displayPhones = (phones,isShowAll) => {
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div class="card-actions justify-end">
-                        <button class="btn btn-accent">Buy Now</button>
+                        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-accent">Show Details</button>
                     </div>
                 </div>
         `;
@@ -50,12 +50,44 @@ const displayPhones = (phones,isShowAll) => {
     // hide loading spinner
     toggleLoadingSpinner(false);
 }
+// handle show details and modal
+ const handleShowDetails= async (id)=>{
+//   console.log('btnclick',id)
+//   load single phone data
+const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+const data = await res.json();
+const phone = data.data;
+showPhoneDetails(phone);
+ }
 
+//  modal section
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+
+const showDetailContainer = document.getElementById('show-detail-container');
+showDetailContainer.innerHTML=`
+<div class="grid place-content-center">
+<h3 class="text-3xl font-bold">${phone.name}</h3>
+<img src="${phone.image}" alt="">
+</div>
+<p><span class="font-bold text-lg">Storage: </span>${phone.mainFeatures.storage}</p>
+<p><span class="font-bold text-lg">Display Size: </span>${phone.mainFeatures.displaySize}</p>
+<p><span class="font-bold text-lg">Chipset: </span>${phone.mainFeatures.chipSet}</p>
+<p><span class="font-bold text-lg">memory: </span>${phone.mainFeatures.memory}</p>
+<p><span class="font-bold text-lg">releaseDate: </span>${phone?.releaseDate}</p>
+<p><span class="font-bold text-lg">Slug: </span>${phone.slug}</p>
+<p><span class="font-bold text-lg">Band: </span>${phone.brand}</p>
+<p><span class="font-bold text-lg">Gps: </span>${phone.others.GPS}</p>
+
+`;
+    //show the modal
+    show_details_modal.showModal()
+}
 // handle search button
 
 const handleSearch = (isShowAll) =>{
     // console.log('search');
-    toggleLoadingSpinner(true);
+toggleLoadingSpinner(true);
 const searchField = document.getElementById('search-field');
 const searchText = searchField.value;
 console.log (searchText);
